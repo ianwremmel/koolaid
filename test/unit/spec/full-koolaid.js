8d131cbafa29eac18371c7c86198c86f0761abc5
Ian W. Remmel <design@ianwremmel.com>
2015-11-08 21:29:42 -0800

    style(test): enable eslint in unit tests

--- test/unit/spec/full-koolaid.js
+++ test/unit/spec/full-koolaid.js
@@ -1,149 +1,145 @@
-/* eslint-disable */
-
-import {assert} from 'chai'
-import {access, method, param, resource} from '../../..'
-import {getRoutingTable, getFlatRoutingTable} from '../../../src/lib/routing-table'
+import {assert} from 'chai';
+import {getRoutingTable, getFlatRoutingTable} from '../../../src/lib/routing-table';
 import A from '../fixtures/a';
 import AA from '../fixtures/aa';
-import AAA from '../fixtures/aaa';
 
-describe('full-koolaid', () => {
-  describe('@resource', () => {
+describe(`full-koolaid`, () => {
+  describe(`@resource`, () => {
 
-    it('produces the correct routing table for a resource', () => {
+    it(`produces the correct routing table for a resource`, () => {
       assert.deepEqual(getRoutingTable(A), {
         methods: {
           create: {
             true: [{
               params: [{
-                source: 'body'
+                source: `body`
               }],
-              accessType: 'write',
+              accessType: `write`,
               after: undefined,
-              path: '/',
-              verb: 'post',
+              path: `/`,
+              verb: `post`
             }]
           },
           update: {
             true: [{
               params: [{
-                source: 'body'
+                source: `body`
               }, {
-                name: 'filter',
-                source: 'query'
+                name: `filter`,
+                source: `query`
               }],
-              accessType: 'write',
+              accessType: `write`,
               after: undefined,
-              path: '/',
-              verb: 'post'
+              path: `/`,
+              verb: `post`
             }],
             false: [{
               params: [{
-                source: 'body'
+                source: `body`
               }],
-              accessType: 'write',
+              accessType: `write`,
               after: undefined,
-              path: '/:id',
-              verb: 'put'
+              path: `/:id`,
+              verb: `put`
             }]
           }
         },
-        basePath: 'a',
-        idParam: 'id'
+        basePath: `a`,
+        idParam: `id`
       });
     });
 
-    it('produces the correct routing table for a derived resource', () => {
+    it(`produces the correct routing table for a derived resource`, () => {
       assert.deepEqual(getRoutingTable(AA), {
         methods: {
           create: {
             true: [{
               params: [{
-                source: 'body'
+                source: `body`
               }],
-              accessType: 'write',
+              accessType: `write`,
               after: undefined,
-              path: '/',
-              verb: 'post',
+              path: `/`,
+              verb: `post`
             }]
           },
           update: {
             true: [{
               params: [{
-                source: 'body'
+                source: `body`
               }, {
-                name: 'filter',
-                source: 'query'
+                name: `filter`,
+                source: `query`
               }],
-              accessType: 'write',
+              accessType: `write`,
               after: undefined,
-              path: '/',
-              verb: 'post'
+              path: `/`,
+              verb: `post`
             }],
             false: [{
               params: [{
-                source: 'body'
+                source: `body`
               }],
-              accessType: 'write',
+              accessType: `write`,
               after: undefined,
-              path: '/:id',
-              verb: 'put'
+              path: `/:id`,
+              verb: `put`
             }]
           },
           exists: {
             true: [{
-              accessType: 'read',
+              accessType: `read`,
               after: undefined,
-              path: '/:id/exists',
-              verb: 'get'
+              path: `/:id/exists`,
+              verb: `get`
             }, {
-              accessType: 'read',
+              accessType: `read`,
               after: undefined,
-              path: '/:id',
-              verb: 'head'
+              path: `/:id`,
+              verb: `head`
             }]
           }
         },
-        basePath: 'aa',
-        idParam: 'id'
+        basePath: `aa`,
+        idParam: `id`
       });
     });
 
-    describe('getFlatRoutingTable', () => {
-      it('produces the correct routing table for a resource', () => {
+    describe(`getFlatRoutingTable`, () => {
+      it(`produces the correct routing table for a resource`, () => {
         assert.deepEqual(getFlatRoutingTable(A), [{
-          accessType: 'write',
+          accessType: `write`,
           after: undefined,
-          verb: 'put',
-          path: '/a/:id',
-          methodName: 'update',
+          verb: `put`,
+          path: `/a/:id`,
+          methodName: `update`,
           isStatic: false,
           params: [
-            {source: 'body'}
+            {source: `body`}
           ]
         }, {
-          accessType: 'write',
+          accessType: `write`,
           after: undefined,
-          verb: 'post',
-          path: '/a/',
-          methodName: 'update',
+          verb: `post`,
+          path: `/a/`,
+          methodName: `update`,
           isStatic: true,
           params: [
-            {source: 'body'},
+            {source: `body`},
             {
-              name: 'filter',
-              source: 'query'
+              name: `filter`,
+              source: `query`
             }
           ]
         }, {
-          accessType: 'write',
+          accessType: `write`,
           after: undefined,
-          verb: 'post',
-          path: '/a/',
-          methodName: 'create',
+          verb: `post`,
+          path: `/a/`,
+          methodName: `create`,
           isStatic: true,
           params: [
-            {source: 'body'}
+            {source: `body`}
           ]
         }]);
       });
